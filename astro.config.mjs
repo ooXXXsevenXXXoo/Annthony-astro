c3f24d54a22d3acdd9f0e3040c4e172df59688a7
ooXXXsevenXXXoo <147204188+ooXXXsevenXXXoo@users.noreply.github.com>
2025-11-27 21:58:16 -0500

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: vercel(),
 });
