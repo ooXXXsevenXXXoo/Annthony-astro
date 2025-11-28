7761231c4dd3fd3fb2a8d9f35f008c3f55add97f
ooXXXsevenXXXoo <147204188+ooXXXsevenXXXoo@users.noreply.github.com>
2025-11-28 04:06:45 -0500

    Add Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
-
+// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
-// Asumiendo que quieres el plugin de vite tal como lo tenías:
-import tailwindcss from '@tailwindcss/vite'; 
-
-// https://astro.build/config
+import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+
   vite: {
     plugins: [tailwindcss()]
-  },
-  output: 'server',
-  // ERROR CORREGIDO: El adaptador DEBE ir dentro del array 'integrations'
-  integrations: [vercel()], 
-  // La propiedad 'adapter' ya no es necesaria ni reconocida en esta sintaxis.
+  }
 });
